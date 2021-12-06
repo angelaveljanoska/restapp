3881bdbcc7906f4e71bf91141c9642352b2bc052
angelaveljanoska <angelaveljanoska@yahoo.com>
2021-12-07 00:55:57 +0100

    Add exception handler

--- src/main/java/com/example/webservices/controller/UserController.java
+++ src/main/java/com/example/webservices/controller/UserController.java
@@ -3,7 +3,10 @@ package com.example.webservices.controller;
 import com.example.webservices.model.User;
 import com.example.webservices.service.implementation.UserServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.context.request.WebRequest;
 
 import java.util.List;
 
@@ -15,22 +18,30 @@ public class UserController {
     private UserServiceImpl userService;
 
     @GetMapping("/users")
-    public List<User> getUsers() {
-        return userService.getUsers();
+    public ResponseEntity<List<User>> getUsers() {
+        return ResponseEntity.ok(userService.getUsers());
     }
 
     @PostMapping("/login")
-    public String login(String username, String password) {
-        return userService.login(username, password);
+    public ResponseEntity<String> login(String username, String password) {
+        return ResponseEntity.ok(userService.login(username, password));
     }
 
     @PostMapping("/register")
-    public void register(String username, String firstPassword, String secondPassword) throws Exception {
+    public ResponseEntity<String> register(String username, String firstPassword, String secondPassword) throws Exception {
         userService.register(username, firstPassword, secondPassword);
+        return ResponseEntity.ok("Successfull registration!");
     }
 
     @DeleteMapping("unregister")
-    public void unregister(String username, String firstPassword, String secondPassword) throws Exception {
+    public ResponseEntity<String> unregister(String username, String firstPassword, String secondPassword) throws Exception {
         userService.unregister(username, firstPassword, secondPassword);
+        return ResponseEntity.ok("Successfull removal!");
     }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleException(Exception ex, WebRequest req) {
+        return new ResponseEntity(ex, HttpStatus.BAD_REQUEST);
+    }
+
 }
