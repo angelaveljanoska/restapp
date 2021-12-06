f76e9e5fe5b6c4cff50b3d5c3240e8774409fa91
angelaveljanoska <angelaveljanoska@yahoo.com>
2021-12-07 00:06:48 +0100

    Add seed data

---
+++ src/main/resources/import.sql
@@ -0,0 +1,3 @@
+INSERT INTO users (username, password) VALUES ('first', '123');
+INSERT INTO users (username, password) VALUES ('second', '456');
+INSERT INTO users (username, password) VALUES ('third', '789');
